da74ba36f11f593a63262cc1d1bec353c1dd67d8
bigsupersniper <linfeng.fjut@gmail.com>
2015-08-24 15:47:32 +0800

    更新类库引用 修改UI字体为默认字体，win10下中文字符乱码 扩展脚本基类函数 添加应用图标

--- CSTester/Scripts/Preloads/ScriptBase.cs
+++ CSTester/Scripts/Preloads/ScriptBase.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Reflection;
 using System.Collections.Generic;
 
@@ -22,4 +23,48 @@ public class ScriptBase : IScript
             }
         }
     }
+
+    public IScript GetScript(string scriptName)
+    {
+        IScript script = null;
+
+        if (ScriptBootstrap.Scripts != null)
+        {
+            script = ScriptBootstrap.Scripts.FirstOrDefault(i => i.Name.Equals(scriptName));
+        }
+
+        if (script == null)
+        {
+            Console.Out.WriteLine("\r" + DateTime.Now.ToString() + " --> script " + scriptName + " not found !");
+        }
+
+        return script;
+    }
+
+    public IFunction GetFunction(string scriptName, string funcName)
+    {
+        var script = GetScript(scriptName);
+        IFunction func = null;
+        if (script != null)
+        {
+            func = script.Functions.FirstOrDefault(i => i.Name.Equals(funcName));
+        }
+
+        if (func == null)
+        {
+            Console.Out.WriteLine("\r" + DateTime.Now.ToString() + " --> function " + scriptName + " not found !");
+        }
+
+        return func;
+    }
+
+    public void RefreshFunctionJson(string scriptName, string funcName, Action<JObject> callback)
+    {
+        var func = GetFunction(scriptName, funcName);
+        if (func != null && callback != null)
+        {
+            callback(func.Json);
+        }
+    }
+
 }
